470a7689053f32ca5b3b34a01dbf785c5b54d53b
Tamaki <c0a242411c@edu.teu.ac.jp>
2025-11-25 14:36:00 +0900

    練習3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -15,6 +15,16 @@ DELTA={
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+
+def check_bound(rct: pg.Rect):
+    yoko, tate = True, True
+    if rct.left < 0 or WIDTH < rct.right: # 横のチェック
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom: # 縦のチェック
+        tate = False
+    return yoko, tate
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -51,8 +61,15 @@ def main():
         # if key_lst[pg.K_RIGHT]:
         #     sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
-        bb_rct.move_ip(vx,vy)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
+        bb_rct.move_ip(vx,vy)
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
